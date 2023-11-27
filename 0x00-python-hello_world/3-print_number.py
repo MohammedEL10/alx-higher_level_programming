cde756cc6dcebe2f5ee6913f3997a44d1b8e0a2c
MohammedEL10 <mohammedelhathout14@gmail.com>
2023-11-27 13:58:01 -0800

    hello python

--- 0x00-python-hello_world/3-print_number.py
+++ 0x00-python-hello_world/3-print_number.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 number = 98
-# YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
-print(f"Battery street,the number is{number}.")
-f"newline: {ord('\n')}"
+print("[:d]Battery street.format(number))
+
