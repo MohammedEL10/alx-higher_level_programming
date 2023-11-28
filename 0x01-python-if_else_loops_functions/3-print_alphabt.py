81eadb6fec3266ecede9a9ed311888c21431c8b1
MohammedEL10 <mohammedelhathout14@gmail.com>
2023-11-28 14:31:09 -0800

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" print the  alphabet, in lowercase, not followed by a new line."""
+
 for alpha_letters in range(ord('a'), ord('z')+1:
         if alpha_letters == 'e' or alpha_letters == 'q':
         continue
