c31e8e06dc45f7eb30dd0b446a727e081058afcb
MohammedEL10 <mohammedelhathout14@gmail.com>
2023-11-29 06:24:17 -0800

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def comb(L)
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+
+L.append(a)
+L.append(b)
+
+for a in range(2):
+for b in range(2):
+
+    print(L[a],L[b])
