e8db7a6a7ce476f8eecb496ba82487aff53a5bc2
MohammedEL10 <mohammedelhathout14@gmail.com>
2023-11-29 14:30:43 -0800

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def magic_calculation(a, b, c):
-    return (a ** b ** c)
+    print(magic_calculation.magic_calculation(3, 1, 2))
+    print(magic_calculation.magic_calculation(2, 1, 3))
