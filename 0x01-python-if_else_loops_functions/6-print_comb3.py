91e1fabf1c6023fb98b64005f3674c3defbfc083
MohammedEL10 <mohammedelhathout14@gmail.com>
2023-11-29 06:41:07 -0800

    Python - if/else, loops, functions

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-def comb(L)
-a=int(input("enter first number"))
-b=int(input("enter second number"))
 
-L.append(a)
-L.append(b)
-
-for a in range(2):
-for b in range(2):
-
-    print(L[a],L[b])
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
+        else:
+            print("{}{}".format(digit1, digit2), end=", ")
