ce7115a76c076d105af625df701ed205fe71f4f0
MohammedEL10 <mohammedelhathout14@gmail.com>
2023-11-30 07:55:43 -0800

    python-import_modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def add(a, b):
-    a = 1:
-        b = 2:
-            print(f("<a value> + <b value> = <add(a, b) value>")
+    a = 1
+        b = 2
+        print("{} + {} = {}".format(a, b))
