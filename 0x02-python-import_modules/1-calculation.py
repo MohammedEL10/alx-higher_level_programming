67661da897995b7d7292e3217d398672377332f7
MohammedEL10 <mohammedelhathout14@gmail.com>
2023-11-30 12:06:55 -0800

    python-import_modules

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from calculator_1.py import calculator_1:
+    a = 10
+    b = 5
+    def add(a, b):
+        return a + b
+    def substruct(a, b):
+        return a - b
+    def multiply(a, b):
+        return a * b
+    def divide(a, b):
+        return a / b
+    print("add")
+    print("substruct")
+    print("mltiply")
+    print("divide")
