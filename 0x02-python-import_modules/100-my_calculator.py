5ddfcb96a2fbeca0ff94794d8270fc4da71ab350
MohammedEL10 <mohammedelhathout14@gmail.com>
2023-11-30 13:03:46 -0800

    python-import_modules

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from calculator_1.py import add, sub, mul, div
+int(a, b)
+print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+exit(1)
