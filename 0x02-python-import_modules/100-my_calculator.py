bf9d85b582694b4f97014f95b18502012d499d7f
MohammedEL10 <mohammedelhathout14@gmail.com>
2023-11-30 13:23:11 -0800

    python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from calculator_1.py import add, sub, mul, div
-int(a, b)
-print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
-exit(1)
+def add(a, b):
+return (a + b)
+def sub(a, b):
+return (a - b)
+def mul(a, b):
+return (a * b)
+def div(a, b):
+return (a / b)
