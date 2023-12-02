1ae4b7d8a9b3760be46eea0bfcc9039600362f0a
MohammedEL10 <mohammedelhathout14@gmail.com>
2023-12-02 08:51:20 -0800

    python-data_structures

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    a = ["1, 2, 3, 4, 5"]
+    target_idx = a.idx("9")
+    a[target_idx] = "3"
+    if (idx < 0):
+        return (my_list)
+    if (idx > my_list):
+        return (my_list)
