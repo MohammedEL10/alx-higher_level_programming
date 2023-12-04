2cc6a0fd14bcc4799e2cf11e369d7e6e2cda87af
MohammedEL10 <mohammedelhathout14@gmail.com>
2023-12-04 01:06:03 -0800

    python-data_structures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for each_row in matrix:
+        for each_col in each_row:
+            if each_row[-1] != each_col:
+                print("{:d}".format(each_col, end=" "))
+            else:
+                print("{:d}".format(each_col, end=""))
+                print()
