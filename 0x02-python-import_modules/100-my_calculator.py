a6b2ee1502a9790b8890be3838b38b4511e01393
MohammedEL10 <mohammedelhathout14@gmai.com>
2023-12-30 06:57:18 -0800

    python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,9 +1,33 @@
 #!/usr/bin/python3
 def add(a, b):
     return (a + b)
+    """my addition function
+    args:
+    first int a
+    second int b
+    return (a + b)
+    """
 def sub(a, b):
     return (a - b)
+    """my substraction function
+    args:
+    first int a
+    second int b
+    return (a - b)
+    """
 def mul(a, b):
     return (a * b)
+    """my multiplication function
+    args:
+    first int a
+    second int b
+    return (a * b)
+    """
 def div(a, b):
     return (a / b)
+    """my division function
+    args:
+    first int a
+    second int b
+    return (a / b)
+    """
