eaaa48068bdfd53982ff93d03373027e7ff225ed
MohammedEL10 <mohammedelhathout@gmail.com>
2024-01-04 04:17:52 -0800

    python-more_classes

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,25 +1,6 @@
 #!/usr/bin/python3
-class Reactangle:
-    """A Rectangle with a width and height."""
-    def __int__(self, w, h):
-        """ (Rectangle, number, number)
-        Create a new rectange of width w and height h.
+"""new class Rectangle"""
 
-        >>> r = Rectangle(1, 2)
-        >>> r.width
-        1
-        >>> r.height
-        2
-        """
-        self.width = w
-        self.height = h
-        def area(self):
-            """(Rectangle) ->number
-            Return the area of this rectangle.
-            >>> r = Rectangle(10, 20)
-            >>> r.area()
-            200
-            """
-
-            return self.width * self.height
-
+class Rectangle:
+    """class Rectangle empty"""
+    pass
