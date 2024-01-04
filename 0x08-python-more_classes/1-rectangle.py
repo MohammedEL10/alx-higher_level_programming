b6e47ec79077470996f32b04df34efed49e3d2d5
MohammedEL10 <mohammedelhathout@gmail.com>
2024-01-04 06:13:14 -0800

    python-more_classes

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" create Rectngle class"""
+class Rectangle:
+    """an empty class Reactangle"""
+    pass
+def width(self):
+    width = int(width)
+    raise:
+        TypeError: width must be an integer
+        if width < 0:
+            raise:
+                ValueError: width must be >= 0
+                def height(self):
+                    height = int(height)
+                    raise:
+                        TypeError: height must be an integer
+                        if height < 0:
+                            raise:
+                                ValueError:height must be >= 0
+                                def __init__(self, width=0, height=0):
