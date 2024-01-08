89399d5116b4f2024ab19da633f3164721c3a197
MohammedEL10 <mohammedelhathout@gmail.com>
2024-01-08 13:44:11 -0800

    python-inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""function that returns True if the object is an instance
-of a class that inherited (directly or indirectly)
-from the specified class ; otherwise False.
-"""
+"""Module containing inherits_from method"""
 
 
 def inherits_from(obj, a_class):
+    """returns True if the object is an instance of a class that
+    inherited (directly or indirectly) from the specified class;
+    otherwise False"""
     return isinstance(obj, a_class) and type(obj) != a_class
--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-""" class BaseGeometry (based on 5-base_geometry.py)"""
+"""BaseGeometry class Module"""
 
 
 class BaseGeometry():
+    """BaseGeometry class"""
     def area(self):
+        """Raises an Exception"""
         raise Exception("area() is not implemented")
