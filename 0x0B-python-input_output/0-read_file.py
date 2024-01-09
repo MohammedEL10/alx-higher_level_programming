dbd4531a635c2abbd923999456803f15e714f7b4
MohammedEL10 <mohammedelhathout@gmail.com>
2024-01-09 13:25:24 -0800

    python-input_output

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""reads a text file (UTF8)"""
+"""defining read_file function"""
 
 
 def read_file(filename=""):
-    with open(filename, encoding='UTF8') as f:
+    """reads filename with utf-8"""
+    with open(filename, encoding='utf-8') as f:
         print(f.read(), end="")
