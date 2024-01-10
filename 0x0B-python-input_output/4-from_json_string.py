ffa7e9377d1f5132828ead1f81675035d9f9d3aa
MohammedEL10 <mohammedelhathout@gmail.com>
2024-01-10 03:12:27 -0800

    python-input_output

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -3,10 +3,8 @@
 
 
 import json
-"""returns an object (Python data structure)
+def from_json_string(my_str):
+    """returns an object (Python data structure)
 represented by a JSON string
 """
-
-
-def from_json_string(my_str):
     return json.loads(my_str)
