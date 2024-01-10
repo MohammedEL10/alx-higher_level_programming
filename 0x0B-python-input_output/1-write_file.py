974f6835b556a668731c84d114036325c1156e3f
MohammedEL10 <mohammedelhathout@gmail.com>
2024-01-10 02:05:07 -0800

    python-input_output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""defining write_file function """
+
+
 def number_of_lines(filename=""):
-    """ function that returns the number of lines of a text file """
-
-
+    """write filename with utf-8"""
     with open(filename, encoding='utf-8') as f:
         return f.write(text)
