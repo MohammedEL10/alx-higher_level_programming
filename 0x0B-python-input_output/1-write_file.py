85425c0260fd64d22407fe79f95130a9bfe08b8d
MohammedEL10 <mohammedelhathout@gmail.com>
2024-01-10 01:43:40 -0800

    python-input_output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,5 +2,6 @@
 def number_of_lines(filename=""):
     """ function that returns the number of lines of a text file """
 
-    with open("filename", "w", encoding="UTF8") as f:
+
+    with open(filename, encoding='utf-8') as f:
         return f.write(text)
---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""function that writes an Object to a text file,
+using a JSON representation:"""
+def save_to_json_file(my_obj, filename):
+    with(filename, "w", encoding = "UTF-8") as f:
+        json.dump((my_obj, f)
