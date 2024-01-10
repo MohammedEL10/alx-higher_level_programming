0adec3e52cc2cac076bc6dd6b42b1661cb4a84d1
MohammedEL10 <mohammedelhathout@gmail.com>
2024-01-10 03:51:00 -0800

    python-input_output

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 """function that writes an Object to a text file,
 using a JSON representation:"""
+
+
+import json
+
+
 def save_to_json_file(my_obj, filename):
-    with(filename, "w", encoding = "UTF-8") as f:
-        json.dump((my_obj, f)
+    """ writes an Object to a text file"""
+
+
+with open(filename, "w+") as f:
+    json.dump(my_obj, f)
---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""module 6-load_from_json_file."""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """ creates an Object from a “JSON file”"""
+
+
+with open(filename, 'r') as f:
+    return json.load(f)
