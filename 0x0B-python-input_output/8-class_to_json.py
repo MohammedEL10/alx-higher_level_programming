3f113e5a7f9174eb4a74482833b7f7c5c4aab16d
MohammedEL10 <mohammedelhathout@gmail.com>
2024-01-10 14:21:23 -0800

    python-input_output

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,9 +2,8 @@
 """ returns the dictionary description with simple data structure
 """
 
-import json
-
 
 def class_to_json(obj):
     """for JSON serialization of an object:
     """
+    return obj.__dict__
