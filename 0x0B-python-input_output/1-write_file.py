8fdae383e8a225aabe232ae2d1aac2328867494c
MohammedEL10 <mohammedelhathout@gmail.com>
2024-01-10 02:14:42 -0800

    python-input_output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,14 @@
 #!/usr/bin/python3
-"""defining write_file function """
+"""Module 1-write_file.
+Writes in a text file.
+"""
 
-
-def number_of_lines(filename=""):
-    """write filename with utf-8"""
-    with open(filename, encoding='utf-8') as f:
+def write_file(filename="", text=""):
+    """Writes text in filename.
+    Args:
+        - filename: name of the file
+        - text: string to write in the file
+    Returns: number of characters written
+    """
+    with open(filename, 'w+') as f:
         return f.write(text)
