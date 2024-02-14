ba4e2ca9404b9bda118c83ad939964f1ca4af737
root <mohammedelhathout14@gmail.com>
2024-02-14 10:19:07 -0800

    0x0E-SQL_more_queries

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -4,4 +4,4 @@
       -- name VARCHAR(256)
    -- The database name will be passed as an argument of the mysql command
    -- If the table unique_id already exists, your script should not fail
-CREATE TABLE IF NOT EXISTS unique_id (id INT, DEFAULT 1, name VARCHAR(256)) NOT NULL);
+CREATE TABLE IF NOT EXISTS unique_id (id INT, DEFAULT 1, UNIQUE (ID), name VARCHAR(256));
