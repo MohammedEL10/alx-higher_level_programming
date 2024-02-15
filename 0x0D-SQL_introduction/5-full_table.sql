be28a4e196d59dbf0324c80ad202cb102315b7b9
root <mohammedelhathout14@gmail.com>
2024-02-15 13:23:11 -0800

    0x0D-SQL_introduction

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,3 +1,3 @@
--- script that prints the full description of the table first_table
--- -- Execute: cat 5-full_table.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
-SHOW CREATE TABELE first_table;
+-- Show the create table statement
+-- Execute: cat 5-full_table.sql | mysql -hlocalhost -uroot -p hbtn_0c_0
+SHOW CREATE TABLE first_table;
