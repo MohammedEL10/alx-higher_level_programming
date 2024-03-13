ace8b490af3c69ff710f776c9d52204cf0108647
MohammedEL10 <mohammedelhathout14@gmail.com>
2024-03-13 11:00:54 -0700

    javascript-warm_up

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,2 +1,10 @@
 #!/usr/bin/node
+// print a square
 
+if (isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  for ( i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log('X'.pepeat(parseInt(process.argv[2])));
+  }
+}
