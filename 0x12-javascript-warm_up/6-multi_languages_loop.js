177dc601e8e105947232568de5509332d1e4c861
MohammedEL10 <mohammedelhathout14@gmail.com>
2024-03-13 07:41:06 -0700

    javascript-warm_up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,7 @@
 const Firstline = 'C is fun';
 const Secondline = 'Python is cool';
 const Thirdline = 'JavaScript is amazing';
-let arr = ["C is fun", "Python is cool", "JavaScript is amazing"];
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < arr.length; i++) {
   console.log(arr[i]);
 }
