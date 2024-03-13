7287aea5a1102d9f11d92544efb71e2766e6aa25
MohammedEL10 <mohammedelhathout14@gmail.com>
2024-03-13 07:35:42 -0700

    javascript-warm_up

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,2 +1,8 @@
 #!/usr/bin/node
-
+const Firstline = 'C is fun';
+const Secondline = 'Python is cool';
+const Thirdline = 'JavaScript is amazing';
+let arr = ["C is fun", "Python is cool", "JavaScript is amazing"];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
