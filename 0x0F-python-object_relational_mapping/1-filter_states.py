f32736fdaa8d5bbc4eb96a627f5abbe745c1c4a4
MohammedEL10 <mohammedelhathout14@gmail.com>
2024-03-15 02:31:16 -0700

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
--- script that lists all states with a name starting with N
+""" lists all states with a name starting with N """
 
 import MySQLdb
 from sys import argv
 if "__name__" == "__main__":
-    conn = MySQLdb.connect(host = localhost; port = 3306; user=argv[1],
+    conn = MySQLdb.connect(host = localhost, port = 3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
