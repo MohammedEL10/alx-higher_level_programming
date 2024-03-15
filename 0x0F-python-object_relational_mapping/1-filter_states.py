f6931addfa6756b4f0de26223842ec59f0756966
MohammedEL10 <mohammedelhathout14@gmail.com>
2024-03-15 03:27:51 -0700

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 """ lists all states with a name starting with N """
-
 import MySQLdb
 from sys import argv
 if "__name__" == "__main__":
-    conn = MySQLdb.connect(host=localhost, port=3306, user=argv[1],
+    # make a connetion with mysql
+    db = MySQLdb.connect(host=localhost, port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
-    cur = conn.cursor()
+    # # It gives us the ability to have multiple seperate working environments
+    # through the same connection to the database.
+    cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY states.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         row[1].startswith("N")
         print(row)
+        # clean up process
         cur.close()
         conn.close()
