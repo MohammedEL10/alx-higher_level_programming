f4aeea21fcc6ec3017da366333ab9004aaa83b9d
MohammedEL10 <mohammedelhathout14@gmail.com>
2024-03-17 02:38:13 -0700

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """
-takes in an argument and displays all values in the states
+Script that takes in an argument and displays all values in the states
 table of hbtn_0e_0_usa where name matches the argument
 """
-
 import MySQLdb
 from sys import argv
 
@@ -18,13 +17,11 @@ if __name__ == '__main__':
     # through the same connection to the database.
     cur = db.cursor()
     nmeSr = "SELECT * FROM states WHERE name LIKE BINARY '{}'".format(argv[4])
-    cur.execute("SELECT * FROM states WHERE name\
-                LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute(nmeSr)
 
     rows = cur.fetchall()
     for i in rows:
         print(i)
     # Clean up process
-
     cur.close()
     db.close()
