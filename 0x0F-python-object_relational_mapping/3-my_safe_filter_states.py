fa1c68d4513a9ba61f22f298453c3c1034c74117
MohammedEL10 <mohammedelhathout14@gmail.com>
2024-03-17 15:12:38 -0700

    python-object_relational_mapping

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
 """
-Wait, do you remember the previous task? Did you test "Arizona';
-TRUNCATE TABLE states ; SELECT * FROM states WHERE
-name = '" as an input?
+Script that takes in an argument and displays all values
+in the states table of hbtn_0e_0_usa where name matches the argument
+but safe from MySQL injections!
 """
 import MySQLdb
 from sys import argv
 
-# the code shold not be executed when imported
-if "__name__" == "__main__":
+# The code should not be executed when imported
+if __name__ == '__main__':
 
     # make a connection to the database
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwod=argv[2], db=argv[3])
+                         passwd=argv[2], db=argv[3])
 
-    # It gives us the ability to have multiple seperate working environment
+    # It gives us the ability to have multiple seperate working environments
     # through the same connection to the database.
-    cur = db.cursur()
-    nmeSr = "SELECT * FROM states WHERE name= %s" [argv[4]]
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE BINARY name = %s", [argv[4]])
 
     rows = cur.fetchall()
     for i in rows:
         print(i)
     # Clean up process
-        cur.close()
-        db.close()
+    cur.close()
+    db.close()
