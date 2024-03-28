50ee4cef7964a8d61bbf9564121fab6e38773c82
MohammedEL10 <mohammedelhathout14@gmail.com>
2024-03-28 04:48:52 -0700

    0x11-python-network_1

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,9 +4,9 @@ import urllib.request
 
 
 if "__name__" == "__main__":
-    with urllib.request.Request("https://alx-intranet.hbtn.io/status") as response:
+    with urllib.request.urlopen("https://alx-intranet.hbtn.io/status") as response:
         body = response.read()
         print("body.response:")
         print("\t- type: {}".format(type(body)))
         print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))
