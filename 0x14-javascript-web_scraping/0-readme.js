588334c5ee0ff6bd54adcf713908214001360ec3
MohammedEL10 <mohammedelhathout14@gmail.com>
2024-04-23 15:06:52 -0700

    avascript-web_scraping#

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,4 +1,10 @@
 #!/usr/bin/node
-const request(node:Path)
-if (err) throw err
-'charset=utf-8'
+// script that reads and prints the content of a file.
+const fs = require('fs')
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    process.stdout.write(data);
+  }
+});
