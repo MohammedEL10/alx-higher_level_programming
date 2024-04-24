d2a94811a75b7c5d01db9e77711861cad9137cb9
MohammedEL10 <mohammedelhathout14@gmail.com>
2024-04-24 07:57:37 -0700

    javascript-web_scraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,12 +1,10 @@
 #!/usr/bin/node
-import { readFileSync } from 'node:fs';
-const fs = require("fs");
-
-
-fs.readFile(process.argv[2], 'utf8', function (err, data) {
+const fs = require('fs');
+const file = process.argv[2];
+fs.readFile(file, 'utf-8', function (err, data) {
   if (err) {
     console.log(err);
   } else {
-    process.stdout.write(data);
+    console.log(data);
   }
 });
